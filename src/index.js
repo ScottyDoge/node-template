15e194a8f370524994096e4a491b070eb733ba27
ppak10 <ppak10@gmail.com>
2020-04-19 14:58:34 -0700

    Adds files for React App

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * Entry file for repository content
  */
 
+// Node Modules
+import React from 'react';
+import {render} from 'react-dom';
+
+// Components
+import App from 'components/App';
+
 // Styles
 import 'styles/main.css';
+
+render(<App />, document.getElementById('root'));
